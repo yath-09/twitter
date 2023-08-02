96b9753a04066fd61dca64eef66701db91895dea
Yatharth <yatharth0903@gmail.com>
2023-08-02 23:58:23 +0530

    repo

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,31 @@
+import {BsHouseFill,BsBellFill} from 'react-icons/bs'
+import {FaUser} from 'react-icons/fa'
+
+const Sidebar=()=>{
+
+const items=[
+    {
+        label:'Home',
+        href:'/',
+        icon: BsHouseFill
+    },
+    {
+        label:'Notifications',
+        href:'/notifications',
+        icon: BsBellFill
+    },
+    {
+        label:'Profile',
+        href:'/users/123',
+        icon: FaUser
+    }
+];
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Sidebar;
